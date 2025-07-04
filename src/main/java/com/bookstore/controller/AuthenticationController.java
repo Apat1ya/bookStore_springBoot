a913500430f28b8b5289d6b9211f0e557c105841
Viktoriia Dragneva <viktoriiadragneva@gmail.com>
2025-07-04 16:08:56 +0300

    Add the login endpoint; Create AuthenticationService, JwtAuthenticationFilter, JwtUtil

--- src/main/java/com/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/bookstore/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.bookstore.controller;
 
+import com.bookstore.dto.user.UserLoginRequestDto;
+import com.bookstore.dto.user.UserLoginResponseDto;
 import com.bookstore.dto.user.UserRegistrationRequestDto;
 import com.bookstore.dto.user.UserResponseDto;
 import com.bookstore.exception.RegistrationException;
+import com.bookstore.service.security.AuthenticationService;
 import com.bookstore.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Authentication", description = "Endpoints for user registration and authentication")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     @Operation(
@@ -28,5 +32,14 @@ public class AuthenticationController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
+    }
+
+    @PostMapping("/login")
+    @Operation(
+            summary = "User Login",
+            description = "Endpoint for authenticating an existing user"
+    )
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto request) {
+        return authenticationService.authenticate(request);
     }
 }
